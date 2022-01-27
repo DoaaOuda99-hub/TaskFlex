9b40389da5eae35c68c96a25264df2beaa0bdde1
DoaaOuda99-hub <wdtd39171@gmail.com>
2022-01-27 12:46:40 +0200

    Flex Task

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+ const App = () => {
+   return (
+   <div className='app'>
+     <div className='Parent'>
+     <div className='containerOne'>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+     </div>
+     <div className='containerTwo'>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+     </div>
+     <div className='containerThird'>
+     <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+     </div>
+     <div className='containerFourth'>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+       <div className='item'></div>
+
+     </div>
+     </div>
+     </div>
+   
+   )
+
+}
+export default App;
